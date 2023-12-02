8faa46cec8ce2fbbdb5492efdf11b9469a8c2f2f
Kevin327 <1779894826@qq.com>
2023-12-02 14:59:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+    <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+  </header>
+
+  <main>
+    <TheWelcome />
+    <el-button type="primary">Primary</el-button>
+  </main> -->
+  <router-view></router-view>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
